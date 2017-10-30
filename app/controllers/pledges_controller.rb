1d916f397c6ef7cde31af8a5a82e82f490920141
AliAlsayed <a_alsayed87@hotmail.com>
2017-10-30 13:03:43 +0400

    add form to make pledges

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -3,4 +3,21 @@ class PledgesController < ApplicationController
 		@project = Project.find(params[:project_id])
 		@pledges = @project.pledges
 	end
+	def new
+		@project = Project.find(params[:project_id])
+		@pledge = @project.pledges.new
+	end
+	def create
+		@project = Project.find(params[:project_id])
+		@pledge = @project.pledges.new(pledge_params)
+		if @pledge.save
+			redirect_to project_pledges_path(@project), notice: 'Thanks for your support'
+		else
+			render :new
+		end
+	end
+	private 
+		def pledge_params
+			params.require(:pledge).permit(:name, :email, :pledge_amount, :comment)
+		end
 end
